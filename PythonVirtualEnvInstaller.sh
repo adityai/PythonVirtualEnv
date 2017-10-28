2ff9f2c1b851044300c94ed77c6cdbe7bfcd581a
Aditya Inapurapu <adityaii@gmail.com>
2017-10-28 09:43:28 -0700

    Switch wget with curl

--- PythonVirtualEnvInstaller.sh
+++ PythonVirtualEnvInstaller.sh
@@ -6,7 +6,7 @@ mkdir ~/.localpython
 
 #Download python tgz
 cd ~/src
-wget http://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+curl -O http://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
 tar -zxvf Python-2.7.10.tgz
 cd Python-2.7.10
 
@@ -18,7 +18,7 @@ make install
 
 #Download virtualenv installer tar.gz
 cd ~/src
-wget https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz#md5=44e19f4134906fe2d75124427dc9b716
+curl -O https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz#md5=44e19f4134906fe2d75124427dc9b716
 tar -zxvf virtualenv-15.1.0.tar.gz
 cd virtualenv-15.1.0/
 
